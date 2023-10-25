2d6f951a879b067f700472a4ca5fd7cf4f7961c1
Edwin Tellman <etellman@pobox.com>
2023-10-25 07:19:20 -0700

    Add partition

--- src/Ch01/Join.hs
+++ src/Ch01/Join.hs
@@ -1,11 +1,13 @@
 module Ch01.Join
-  ( join,
-    join2,
+  ( simplify,
+    partitions,
+    join,
     disjoint,
     connected,
     System (..),
     sets,
     elements,
+    distribute,
   )
 where
 
@@ -32,23 +34,49 @@ instance Eq a => Ord (System a) where
         connectionMatches (x, y) = not (connected x y sx) || connected x y sy
      in all connectionMatches pairs
 
+instance Functor System where
+  fmap :: (a -> b) -> System a -> System b
+  fmap f (System xss) = System ((fmap . fmap) f xss)
+
+distribute :: a -> [[a]] -> [[[a]]]
+distribute x [] = [[[x]]]
+distribute x (xs : xss) =
+  -- 'a' ("bc" : ["de", "fg"])
+  let firstWithX = ((x : xs) : xss) -- ["abc", "de", "fg"]
+      restWithX = distribute x xss -- [["ade"], ["afg"]]
+      prependXs = map (xs :) restWithX -- [[bc, "ade"], [bc, "afg"]]
+   in firstWithX : prependXs -- [["abc", "de", "fg"], [[bc, "ade"], [bc, "afg"]]]
+
+partitions :: [a] -> [[[a]]]
+partitions [] = [[]]
+partitions (x : xs) = do
+  yss <- partitions xs
+  ys <- distribute x yss
+  return ys
+
+-- or
+-- partitions (x : xs) = [ys | yss <- partitions xs, ys <- distribute x yss]
+
+-- or
+-- partitions = foldr (\x r -> r >>= distribute x) [[]]
+
 -- determines whether all the groups contain different elements
 disjoint :: Eq a => System a -> Bool
 disjoint (System (xss)) =
   let elems = concat xss
    in (length . nub) elems == length elems
 
-join2 :: Ord a => System a -> System a -> System a
-join2 (System xss) (System yss) = join $ System (xss ++ yss)
+join :: Ord a => System a -> System a -> System a
+join (System xss) (System yss) = simplify $ System (xss ++ yss)
 
-join :: Ord a => System a -> System a
-join (System []) = (System [])
-join s@(System (x : xss))
+simplify :: Ord a => System a -> System a
+simplify (System []) = (System [])
+simplify s@(System (x : xss))
   | disjoint s = System $ sort $ fmap sort (x : xss)
   | otherwise =
       let (nonOverlapping, overlapping) = partition (\y -> null (intersect x y)) xss
           withX = if null overlapping then [x] else fmap (union x) overlapping
-       in join (System $ nonOverlapping ++ withX)
+       in simplify (System $ nonOverlapping ++ withX)
 
 -- determines whether two numbers are in the same set
 connected :: Eq a => a -> a -> System a -> Bool
--- test/Ch01/JoinTest.hs
+++ test/Ch01/JoinTest.hs
@@ -1,7 +1,7 @@
 module Ch01.JoinTest (tests) where
 
 import Ch01.Join
-import Data.List (nub)
+import Data.List (nub, sort)
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
@@ -9,8 +9,8 @@ import Test.Tasty
 import Test.Tasty.HUnit as HU
 import Test.Tasty.Hedgehog
 
-prop_join :: Property
-prop_join =
+prop_simplify :: Property
+prop_simplify =
   property $ do
     -- set up
     s <-
@@ -22,12 +22,41 @@ prop_join =
             (Gen.list (Range.constant 0 10) (Gen.int $ Range.constant 1 20))
 
     -- exercise
-    let js = join s
+    let js = simplify s
 
     -- verify
     elements js === elements s
     H.assert $ disjoint js
     H.assert $ s <= js
+
+prop_join :: Property
+prop_join =
+  property $ do
+    -- set up
+    s1 <-
+      forAll $
+        System
+          <$> (fmap nub)
+          <$> Gen.list
+            (Range.constant 0 50)
+            (Gen.list (Range.constant 1 10) (Gen.int $ Range.constant 1 20))
+    s2 <-
+      forAll $
+        System
+          <$> (fmap nub)
+          <$> Gen.list
+            (Range.constant 0 50)
+            (Gen.list (Range.constant 1 10) (Gen.int $ Range.constant 1 20))
+
+    let combined = ((sort . nub) $ (elements s1) ++ (elements s2))
+
+    -- exercise
+    let js = join s1 s2
+
+    -- verify
+    elements js === combined
+    H.assert $ disjoint js
+    H.assert $ System (sets s1 ++ sets s2) <= js
 
 tests :: TestTree
 tests =
@@ -38,11 +67,12 @@ tests =
         assertBool "overlap" $ not . disjoint $ System [['a', 'b'], ['b', 'c']]
         assertBool "singleton" $ disjoint $ System [['a']]
         assertBool "empty" $ disjoint $ (System [[]] :: System Char),
+      testProperty "simplify" prop_simplify,
       testProperty "join" prop_join,
       testCase "exercise 1.2" $ do
         let s1 = System [[11, 12], [13], [21], [22, 23]] :: System Int
             s2 = System [[11], [21], [12, 22], [13, 23]]
-        join2 s1 s2 @?= System [[11, 12, 13, 22, 23], [21]],
+        join s1 s2 @?= System [[11, 12, 13, 22, 23], [21]],
       testCase
         "example 1.1.1"
         $ do
@@ -50,5 +80,18 @@ tests =
               s2 = System [['a'], ['b', 'c']]
           assertBool "s1" $ not $ connected 'a' 'c' $ s1
           assertBool "s2" $ not $ connected 'a' 'c' $ s2
-          assertBool "s1 v s2" $ connected 'a' 'c' $ join2 s1 s2
+          assertBool "s1 v s2" $ connected 'a' 'c' $ join s1 s2,
+      testCase "partition" $ do
+        let xs = [1 .. 4] :: [Int]
+        (length $ partitions xs) @?= 15,
+      testGroup
+        "distribute"
+        [ testCase "non-empty" $
+            distribute 'a' ["b", "c", "de"]
+              @=? [["ab", "c", "de"], ["b", "ac", "de"], ["b", "c", "ade"], ["b", "c", "de", "a"]],
+          testCase "empty" $ distribute 'a' [] @=? [["a"]],
+          testCase "bind" $
+            ([["bc", "e"], ["d"]] >>= distribute 'a')
+              @?= [["abc", "e"], ["bc", "ae"], ["bc", "e", "a"], ["ad"], ["d", "a"]]
+        ]
     ]
