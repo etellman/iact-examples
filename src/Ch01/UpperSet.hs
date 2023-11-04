2e1b4c5a74f9452a1e653a4f4b1d0d2a84b33592
Edwin Tellman <etellman@pobox.com>
2023-11-04 08:00:13 -0700

    Refactor upper set test
    Use the Preorder type in the upper set test.

--- src/Ch01/UpperSet.hs
+++ src/Ch01/UpperSet.hs
@@ -3,12 +3,11 @@ module Ch01.UpperSet
   )
 where
 
+import Ch01.Preorder
 import Data.List (nub)
 
 isUpperSet :: Eq a => (a -> a -> Bool) -> [a] -> [a] -> Bool
 isUpperSet gte elements set = all (\x -> x `elem` set || (not $ any (gte x) set)) elements
 
-upperSets :: Eq a => (a -> a -> Bool) -> [[a]] -> [[a]]
-upperSets gte xss =
-  let elements = (nub . concat) xss
-   in filter (isUpperSet gte elements) xss
+upperSets :: Eq a => Preorder a -> [[a]] -> [[a]]
+upperSets (Preorder gte elements) xss = filter (isUpperSet gte elements) xss
--- test/Ch01/UpperSetTest.hs
+++ test/Ch01/UpperSetTest.hs
@@ -1,8 +1,8 @@
 module Ch01.UpperSetTest (tests) where
 
+import Ch01.Preorder
 import Ch01.UpperSet
 import Control.Monad (filterM)
-import Data.List (nub)
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import Test.Tasty
@@ -10,10 +10,9 @@ import Test.Tasty.HUnit
 import Test.Tasty.Hedgehog
 import TestLib.Assertions
 
-prop_upperSets :: (Show a, Eq a) => [[a]] -> (a -> a -> Bool) -> Property
-prop_upperSets xss gte = property $ do
+prop_upperSets :: (Show a, Eq a) => Preorder a -> [[a]] -> Property
+prop_upperSets (Preorder gte elements) xss = property $ do
   -- set up
-  let elements = (nub . concat) xss
   set <- forAll $ Gen.element $ filter (not . null) xss
 
   x <- forAll $ Gen.element elements
@@ -30,18 +29,21 @@ tests =
         "Binary preorder"
         [ testProperty "all upper sets" $ do
             let xss = [[True], [False, True]]
-            prop_upperSets xss (>=),
+            prop_upperSets (Preorder (>=) [False, True]) xss,
           testCase "filter upper sets" $ do
-            let xss = filterM (const [False, True]) [False, True]
-            upperSets (>=) xss @?= [[], [True], [False, True]]
+            let elements = [False, True]
+                xss = filterM (const [False, True]) elements
+            upperSets (Preorder (>=) elements) xss @?= [[], [True], [False, True]]
         ],
       testGroup
         "discrete preorder"
         [ testProperty "all upper sets" $ do
-            let xss = filterM (const [False, True]) [1 .. 10] :: [[Int]]
-            prop_upperSets xss (\_ _ -> False),
+            let elements = [1 .. 10] :: [Int]
+                xss = filterM (const [False, True]) elements
+            prop_upperSets (Preorder (\_ _ -> False) elements) xss,
           testCase "filter upper sets" $ do
-            let xss = filterM (const [False, True]) [1 .. 3] :: [[Int]]
-            upperSets (\_ _ -> False) xss @?= xss
+            let elements = [1 .. 10] :: [Int]
+                xss = filterM (const [False, True]) elements
+            upperSets (Preorder (\_ _ -> False) elements) xss @?= xss
         ]
     ]
