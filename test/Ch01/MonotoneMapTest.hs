2318c7eb4dd600ab02644c7c583f6721e7e73afc
Edwin Tellman <etellman@pobox.com>
2023-11-10 07:08:27 -0800

    Add exercise 61 part 4

--- test/Ch01/MonotoneMapTest.hs
+++ test/Ch01/MonotoneMapTest.hs
@@ -9,8 +9,15 @@ import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.Hedgehog
 import TestLib.Assertions
+
+-- comparison function for exercise 61 part 4
+lte61 :: Char -> Char -> Bool
+lte61 'a' 'b' = True
+lte61 'a' 'c' = True
+lte61 x y = x == y
 
 prop_example55 :: Property
 prop_example55 = property $ do
@@ -44,13 +51,13 @@ prop_exercise61_1 = property $ do
   let ap = arrow (<=) p xss
 
   -- verify
-  assert $ isUpperSet (>=) ap xss
+  H.assert $ isUpperSet (>=) ap xss
 
-prop_exercise61_23 :: Property
-prop_exercise61_23 = property $ do
+prop_exercise61_2_3 :: (Show a, Eq a) => (a -> a -> Bool) -> Gen [a] -> Property
+prop_exercise61_2_3 lte generator = property $ do
   -- set up
-  xs <- forAll $ nub <$> Gen.list (Range.linear 1 20) Gen.alpha
-  let po = P.Preorder (<=) xs
+  xs <- forAll $ nub <$> generator
+  let po = P.Preorder lte xs
 
   -- exercise
   let arrowPreorder = arrowMonotoneMap po
@@ -58,10 +65,10 @@ prop_exercise61_23 = property $ do
   -- verify
   p <- forAll $ Gen.element xs
   q <- forAll $ Gen.element xs
-  let arrow' x = arrow (<=) x xs
+  let arrow' x = arrow lte x xs
 
   P.isLte (P.opposite po) p q ==> P.isLte arrowPreorder (arrow' p) (arrow' q)
-  (p <= q) === ((arrow' q) `isSubsetOf` (arrow' p))
+  (p `lte` q) === ((arrow' q) `isSubsetOf` (arrow' p))
 
 tests :: TestTree
 tests =
@@ -72,6 +79,20 @@ tests =
       testGroup
         "exercise 61"
         [ testProperty "part 1" prop_exercise61_1,
-          testProperty "parts 2 and 3" prop_exercise61_23
+          testProperty "parts 2 and 3" $
+            prop_exercise61_2_3 (<=) (Gen.list (Range.linear 1 20) Gen.alpha),
+          testProperty "parts 4 with part 2 and 3 properties" $ do
+            prop_exercise61_2_3 lte61 (Gen.constant ['a' .. 'c']),
+          testCase "part 4" $ do
+            -- set up
+            let xs = ['a' .. 'c']
+                pairs = fmap (\x -> (x, arrow lte61 x xs)) xs
+
+            -- exercise and verify
+            pairs
+              @=? [ ('a', "abc"),
+                    ('b', "b"),
+                    ('c', "c")
+                  ]
         ]
     ]
