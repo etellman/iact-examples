a41218ffc3a79f30b1dc583fe5b3bb98e0b048e2
Edwin Tellman <etellman@pobox.com>
2023-11-23 06:23:28 -0800

    Extract assertMeet
    Extract assertMeet function into Assertions module.

--- test/Ch01/Sec3/Definition76Test.hs
+++ test/Ch01/Sec3/Definition76Test.hs
@@ -1,5 +1,6 @@
 module Ch01.Sec3.Definition76Test (tests) where
 
+import Ch01.Preorder (Preorder (..))
 import Data.Set (toList)
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
@@ -14,18 +15,17 @@ prop_definition76 = property $ do
   let from = 1
       to = 1000
 
-  as <- forAll $ Gen.set (Range.constant 1 20) (Gen.int $ (Range.linear from to))
+  let xs = [from .. to]
+      po = Preorder (<=) xs
+
+  as <- forAll $ toList <$> Gen.set (Range.constant 1 20) (Gen.int $ (Range.linear from to))
   n <- forAll $ Gen.int (Range.constant 0 5)
 
   -- select a meet
   let p = max from (minimum as - n)
 
   -- verify meet properties
-  a <- forAll $ Gen.element $ toList as
-  H.assert $ p <= a
-
-  q <- forAll $ Gen.int (Range.constant from to)
-  a <= q ==> p <= q
+  assertMeet po as p
 
 tests :: TestTree
 tests = testProperty "Ch01.Sec3.Definition76Test" prop_definition76
