14bfe7f46091330eefaddee2d61335e082273f25
Edwin Tellman <etellman@pobox.com>
2023-11-26 06:03:30 -0800

    Use monoids Any and All in example 1.83

--- test/Ch01/Sec3/Example83Test.hs
+++ test/Ch01/Sec3/Example83Test.hs
@@ -1,6 +1,7 @@
 module Ch01.Sec3.Example83Test (tests) where
 
 import Ch01.Preorder (Preorder (..))
+import Data.Monoid (All (..), Any (..))
 import Data.Set (fromList, toList)
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
@@ -9,21 +10,27 @@ import Test.Tasty.Hedgehog
 import TestLib.Assertions
 
 prop_booleans ::
-  (Bool -> Bool -> Bool) ->
-  Bool ->
+  Monoid a =>
+  (Bool -> a) ->
+  (a -> Bool) ->
   (Preorder Bool -> [Bool] -> Bool -> PropertyT IO ()) ->
   Property
-prop_booleans combine initial verify = property $ do
+prop_booleans fromBool toBool verify = property $ do
+  -- set up
   let xs = [True, False]
   xs' <- forAll $ toList <$> Gen.subset (fromList xs)
 
-  verify (Preorder (<=) xs) xs' (foldr combine initial xs')
+  -- exercise
+  let meet = toBool $ mconcat (fmap fromBool xs')
+
+  -- verify
+  verify (Preorder (<=) xs) xs' meet
 
 prop_meet :: Property
-prop_meet = prop_booleans (&&) True assertMeet
+prop_meet = prop_booleans All getAll assertMeet
 
 prop_join :: Property
-prop_join = prop_booleans (||) False assertJoin
+prop_join = prop_booleans Any getAny assertJoin
 
 tests :: TestTree
 tests =
