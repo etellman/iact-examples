799f769ac953ba7a08c2e450b55648a2b1129107
Edwin Tellman <etellman@pobox.com>
2023-11-28 07:03:15 -0800

    Add example 1.91

---
+++ test/Ch1/Sec4/Example91Test.hs
@@ -0,0 +1,30 @@
+module Ch1.Sec4.Example91Test (tests) where
+
+import Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+divide :: Int -> Int -> Int
+divide n x = ceiling ((fromIntegral x / fromIntegral n) :: Double)
+
+prop_galois :: Property
+prop_galois = property $ do
+  -- set up
+  n <- forAll $ Gen.int (Range.linear 2 100)
+  let g = (n *)
+      f = divide n
+
+  x <- forAll $ Gen.int (Range.linear 1 1000)
+  y <- forAll $ Gen.int (Range.linear 1 1000)
+
+  -- exercise and verify
+  (f x <= y) === (x <= g y)
+
+tests :: TestTree
+tests =
+  testGroup
+    "Ch1.Sec4.Example91Test"
+    [ testProperty "galois" prop_galois
+    ]
--- test/Ch1/Sec4/SectionTest.hs
+++ test/Ch1/Sec4/SectionTest.hs
@@ -1,9 +1,11 @@
 module Ch1.Sec4.SectionTest (sectionTests) where
 
+import Ch1.Sec4.Example91Test
 import Test.Tasty
 
 sectionTests :: TestTree
 sectionTests =
   testGroup
     "Ch1.Sec4.SectionTest"
-    []
+    [ Ch1.Sec4.Example91Test.tests
+    ]
