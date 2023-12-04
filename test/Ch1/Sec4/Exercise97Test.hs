c04df52987570f9585d87d11cb60839615c06b90
Edwin Tellman <etellman@pobox.com>
2023-12-04 08:19:13 -0800

    Add exercise 97 right adjunct

--- test/Ch1/Sec4/Exercise97Test.hs
+++ test/Ch1/Sec4/Exercise97Test.hs
@@ -6,6 +6,7 @@ import Ch1.Set
     overlapsBy,
     sameElementsBy,
   )
+import Data.List (nub)
 import Data.Set (toList)
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
@@ -18,29 +19,43 @@ newtype S = S Int deriving (Show, Eq, Ord)
 
 newtype T = T Int deriving (Show, Eq, Ord)
 
--- | verifies that g creates a Galois connection between S and T
-checkGaloisConnection :: [[S]] -> (S -> T) -> PropertyT IO ()
-checkGaloisConnection sss g = do
+samePartition :: Eq a => [[a]] -> a -> a -> Bool
+samePartition xss x1 x2 = partitionFor xss x1 == partitionFor xss x2
+
+checkRightAdjunct :: [S] -> [[T]] -> (S -> T) -> PropertyT IO ()
+checkRightAdjunct ss tss g = do
+  -- exercise
+  let ssFor t = filter (\s -> g s == t) ss
+      sss = concat $ (fmap . fmap) ssFor tss
+
+  -- verify
+  H.assert $ sameElementsBy (==) (concat sss) ss
+  overlapsBy (==) sss === []
+
+  s1 <- forAll $ Gen.element ss
+  s2 <- forAll $ Gen.element ss
+  cover 20 "same S partition" (samePartition sss s1 s2)
+
+  (samePartition sss s1 s2) ==> samePartition tss (g s1) (g s2)
+
+checkLeftAdjunct :: [[S]] -> (S -> T) -> PropertyT IO ()
+checkLeftAdjunct sss g = do
   -- exercise
   let tss = closureBy (==) $ (fmap . fmap) g sss
       ss = concat sss
 
   -- verify
-  s1 <- forAll $ Gen.element ss
-  s2 <- forAll $ Gen.element ss
-
-  let samePartition xss x1 x2 = partitionFor xss x1 == partitionFor xss x2
-  cover 20 "same S partition" (samePartition sss s1 s2)
-
-  -- tss is a partition of T
   H.assert $ sameElementsBy (==) (concat tss) (fmap g ss)
   overlapsBy (==) tss === []
 
-  -- everything in S is in the right place in T
+  s1 <- forAll $ Gen.element ss
+  s2 <- forAll $ Gen.element ss
+  cover 20 "same S partition" (samePartition sss s1 s2)
+
   (samePartition sss s1 s2) ==> samePartition tss (g s1) (g s2)
 
-prop_example96 :: Property
-prop_example96 = property $ do
+prop_example96Left :: Property
+prop_example96Left = property $ do
   -- set up
   let sss = [[S 1, S 3], [S 2, S 4]]
       g (S 1) = (T 12)
@@ -48,30 +63,67 @@ prop_example96 = property $ do
       g (S s) = (T s)
 
   -- exercise
-  checkGaloisConnection sss g
+  checkLeftAdjunct sss g
 
-prop_exercise97 :: Property
-prop_exercise97 = property $ do
+prop_example96Right :: Property
+prop_example96Right = property $ do
   -- set up
-  ss <-
-    forAll $
-      fmap S
-        <$> toList
-        <$> Gen.set
-          (Range.constant 2 10)
-          (Gen.int $ (Range.linear 0 1000))
-  sss <- forAll $ Gen.element (partitions ss)
+  let ss = fmap S [1 .. 4]
+      g (S 1) = (T 12)
+      g (S 2) = (T 12)
+      g (S s) = (T s)
+      tss = [fmap T [3, 4, 12]]
 
+  -- exercise
+  checkRightAdjunct ss tss g
+
+genExercise97Ss :: Gen [S]
+genExercise97Ss =
+  fmap S
+    <$> toList
+    <$> Gen.set
+      (Range.constant 2 10)
+      (Gen.int $ (Range.linear 0 1000))
+
+exercise97g :: PropertyT IO (S -> T)
+exercise97g = do
   n <- forAll $ Gen.int (Range.constant 2 20)
   let g (S s) = T (s `rem` n)
 
+  return g
+
+prop_exercise97Left :: Property
+prop_exercise97Left = property $ do
+  -- set up
+  ss <- forAll genExercise97Ss
+  sss <- forAll $ Gen.element (partitions ss)
+  g <- exercise97g
+
   -- exercise
-  checkGaloisConnection sss g
+  checkLeftAdjunct sss g
+
+prop_exercise97Right :: Property
+prop_exercise97Right = property $ do
+  -- set up
+  ss <- forAll genExercise97Ss
+  g <- exercise97g
+  tss <- forAll $ Gen.element $ (partitions . nub . fmap g) ss
+
+  -- exercise
+  checkRightAdjunct ss tss g
 
 tests :: TestTree
 tests =
   testGroup
     "Ch1.Sec4.Exercise97Test"
-    [ testProperty "example 96" prop_example96,
-      testProperty "exercise 97" prop_exercise97
+    [ testGroup
+        "example 96"
+        [ testProperty "left" prop_example96Left,
+          testProperty "right" prop_example96Right
+        ],
+      testGroup
+        "exercise 97"
+        [ testProperty "left" prop_exercise97Left,
+          testProperty "right" prop_exercise97Right
+        ]
     ]
