211523463295f62e4c8a7b87423400c56122951b
Edwin Tellman <etellman@pobox.com>
2023-12-14 07:38:00 -0800

    Delete Ch1.Preorder

--- test/Ch1/ChapterTest.hs
+++ test/Ch1/ChapterTest.hs
@@ -3,7 +3,6 @@ module Ch1.ChapterTest (chapterTests) where
 import Ch1.BooleanSystemTest
 import Ch1.GraphTest
 import Ch1.PartitionTest
-import Ch1.PreorderTest
 import Ch1.PreservingTest
 import Ch1.Sec1.SectionTest
 import Ch1.Sec2.SectionTest
@@ -28,6 +27,5 @@ chapterTests =
       Ch1.SetTest.tests,
       Ch1.GraphTest.tests,
       Ch1.UpperSetTest.tests,
-      Ch1.PreorderTest.tests,
       Ch1.PartitionTest.tests
     ]
