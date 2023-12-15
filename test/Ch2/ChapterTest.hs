35ec71931799b5b70f2f9ec908933d55fdad03d0
Edwin Tellman <etellman@pobox.com>
2023-12-15 06:37:47 -0800

    Rename Ch1.Sec1 to Ch2.Sec2
    There aren't any exercises in section 2.1.

--- test/Ch2/ChapterTest.hs
+++ test/Ch2/ChapterTest.hs
@@ -1,11 +1,11 @@
 module Ch2.ChapterTest (chapterTests) where
 
-import Ch2.Sec1.SectionTest
+import Ch2.Sec2.SectionTest
 import Test.Tasty
 
 chapterTests :: TestTree
 chapterTests =
   testGroup
     "Ch2.ChapterTest"
-    [ Ch2.Sec1.SectionTest.sectionTests
+    [ Ch2.Sec2.SectionTest.sectionTests
     ]
--- test/Ch2/Sec2/SectionTest.hs
+++ test/Ch2/Sec2/SectionTest.hs
@@ -1,9 +1,9 @@
-module Ch2.Sec1.SectionTest (sectionTests) where
+module Ch2.Sec2.SectionTest (sectionTests) where
 
 import Test.Tasty
 
 sectionTests :: TestTree
 sectionTests =
   testGroup
-    "Ch2.Sec1.SectionTest"
+    "Ch2.Sec2.SectionTest"
     []
