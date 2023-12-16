76b3d963e609b71b1c4360d25121402ca1b4ef25
Edwin Tellman <etellman@pobox.com>
2023-12-16 07:20:54 -0800

    Add exercise 6

---
+++ test/Ch2/Sec2/Exercise6Test.hs
@@ -0,0 +1,25 @@
+module Ch2.Sec2.Exercise6Test (tests) where
+
+import Ch2.MpoProperties
+import Ch2.Sec2.Exercise6
+import Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+genReal :: Gen RealDiscrete
+genReal = RealDiscrete <$> Gen.realFloat (Range.exponentialFloat (-1000) 1000)
+
+-- | only a few choices so the monotonicity test will find elements equal to each other
+genRealNarrow :: Gen RealDiscrete
+genRealNarrow = Gen.element $ map RealDiscrete [0, 1]
+
+tests :: TestTree
+tests =
+  testGroup
+    "Ch2.Sec2.Exercise6Test"
+    [ monoid "monoid" genReal,
+      testProperty "symmetry" $ prop_symmetry genReal,
+      testProperty "monotonicity only non-negative" $ prop_monotonicity genRealNarrow
+    ]
--- test/Ch2/Sec2/SectionTest.hs
+++ test/Ch2/Sec2/SectionTest.hs
@@ -2,6 +2,7 @@ module Ch2.Sec2.SectionTest (sectionTests) where
 
 import Ch2.Sec2.Example3Test
 import Ch2.Sec2.Exercise4Test
+import Ch2.Sec2.Exercise6Test
 import Test.Tasty
 
 sectionTests :: TestTree
@@ -9,5 +10,6 @@ sectionTests =
   testGroup
     "Ch2.Sec2.SectionTest"
     [ Ch2.Sec2.Example3Test.tests,
-      Ch2.Sec2.Exercise4Test.tests
+      Ch2.Sec2.Exercise4Test.tests,
+      Ch2.Sec2.Exercise6Test.tests
     ]
