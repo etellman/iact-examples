b4e6f493d851b50787596fbacb007f1fd9b3db74
Edwin Tellman <etellman@pobox.com>
2023-12-16 06:30:12 -0800

    Add exercise 2.4

---
+++ test/Ch2/Sec2/Exercise4Test.hs
@@ -0,0 +1,24 @@
+module Ch2.Sec2.Exercise4Test (tests) where
+
+import Ch2.MpoProperties
+import Ch2.Sec2.Exercise4
+import Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+genReal :: ApproximateDouble -> Gen RealTimes
+genReal from = RealTimes <$> Gen.realFloat (Range.exponentialFloat from 1000)
+
+tests :: TestTree
+tests =
+  testGroup
+    "Ch2.Sec2.Exercise4Test"
+    [ testGroup
+        "all reals"
+        [ monoid "monoid" $ genReal (-1000),
+          testProperty "symmetry" $ prop_symmetry $ genReal (-1000)
+        ],
+      testProperty "monotonicity only non-negative" $ prop_monotonicity $ genReal 0
+    ]
--- test/Ch2/Sec2/SectionTest.hs
+++ test/Ch2/Sec2/SectionTest.hs
@@ -1,11 +1,13 @@
 module Ch2.Sec2.SectionTest (sectionTests) where
 
 import Ch2.Sec2.Example3Test
+import Ch2.Sec2.Exercise4Test
 import Test.Tasty
 
 sectionTests :: TestTree
 sectionTests =
   testGroup
     "Ch2.Sec2.SectionTest"
-    [ Ch2.Sec2.Example3Test.tests
+    [ Ch2.Sec2.Example3Test.tests,
+      Ch2.Sec2.Exercise4Test.tests
     ]
