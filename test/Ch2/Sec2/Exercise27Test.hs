fe08eb1ce99ab29e8c636f7fa8b7ab31f9bb0004
Edwin Tellman <etellman@pobox.com>
2023-12-23 06:48:27 -0800

    Add exercise 2.27

---
+++ test/Ch2/Sec2/Exercise27Test.hs
@@ -0,0 +1,27 @@
+module Ch2.Sec2.Exercise27Test (tests) where
+
+import Ch2.MonoidalMapProperties
+import Ch2.Sec2.BooleanMonoids
+import Ch2.Sec2.CostMonoid
+import Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import Test.Tasty
+
+genBool :: Gen BooleanAnd
+genBool = BooleanAnd <$> Gen.bool
+
+boolToCost :: BooleanAnd -> CostPreorder
+boolToCost (BooleanAnd False) = CostPreorder Infinity
+boolToCost (BooleanAnd True) = CostPreorder $ Cost 0
+
+tests :: TestTree
+tests =
+  testGroup
+    "Ch2.Sec2.Exercise27Test"
+    [ testGroup
+        "Boolean -> Cost"
+        [ laxMonotoneMap genBool boolToCost,
+          strongMonotoneMap genBool boolToCost,
+          strictMonotoneMap genBool boolToCost
+        ]
+    ]
--- test/Ch2/Sec2/SectionTest.hs
+++ test/Ch2/Sec2/SectionTest.hs
@@ -6,6 +6,7 @@ import Ch2.Sec2.Example21Test
 import Ch2.Sec2.Example26Test
 import Ch2.Sec2.Example3Test
 import Ch2.Sec2.Exercise10Test
+import Ch2.Sec2.Exercise27Test
 import Ch2.Sec2.Exercise13Test
 import Ch2.Sec2.Exercise15Test
 import Ch2.Sec2.Exercise17Test
@@ -33,5 +34,6 @@ sectionTests =
       Ch2.Sec2.Exercise19Test.tests,
       Ch2.Sec2.Example21Test.tests,
       Ch2.Sec2.Exercise24Test.tests,
-      Ch2.Sec2.Example26Test.tests
+      Ch2.Sec2.Example26Test.tests,
+      Ch2.Sec2.Exercise27Test.tests
     ]
