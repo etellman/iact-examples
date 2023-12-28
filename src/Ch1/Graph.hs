4ca553f4afb4bed5403decaa08cc4cc4f247caff
Edwin Tellman <etellman@pobox.com>
2023-12-28 08:08:00 -0800

    Extract filter method in connections

--- src/Ch1/Graph.hs
+++ src/Ch1/Graph.hs
@@ -14,7 +14,9 @@ class Graph v a | v -> a where
 
 -- direct connections between two vertices
 connections :: (Eq v, Graph v a) => v -> v -> [a]
-connections v1 v2 = filter (\x -> target x == v2) (arrowsFrom v1)
+connections v1 v2 =
+  let targetMatches a = target a == v2
+    in filter targetMatches (arrowsFrom v1)
 
 -- determine if there is at least one path between two vertices
 path :: (Eq v, Graph v a) => v -> v -> Bool
