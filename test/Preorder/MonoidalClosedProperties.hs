6776ee550e5fcc06c87d692cabff494e2d5b0a2b
Edwin Tellman <etellman@pobox.com>
2024-01-13 07:42:55 -0800

    Add proposition 2.64 (d)

--- test/Preorder/MonoidalClosedProperties.hs
+++ test/Preorder/MonoidalClosedProperties.hs
@@ -6,18 +6,30 @@ import Preorder.Preorder as PO
 import Test.Tasty
 import Test.Tasty.Hedgehog
 
-testC ::
+prop64c ::
   (Monoid m, Show m, PO.Preorder m) =>
   Gen m ->
   (m -> m -> m) ->
   Property
-testC gen (-*) = property $ do
+prop64c gen (-*) = property $ do
   -- set up
   v <- forAll gen
   w <- forAll gen
 
   -- exercise and verify
   H.assert $ (v <> (v -* w)) PO.<= w
+
+prop64d ::
+  (Monoid m, Show m, PO.Preorder m) =>
+  Gen m ->
+  (m -> m -> m) ->
+  Property
+prop64d gen (-*) = property $ do
+  -- set up
+  v <- forAll gen
+
+  -- exercise and verify
+  H.assert $ v PO.=~ (mempty -* v)
 
 testClosed ::
   (Monoid m, Show m, PO.Preorder m) =>
@@ -37,7 +49,8 @@ testClosed name gen (-*) v =
               namedLaxMonotoneMap "right" gen rightAdjunct
             ],
           testGroup
-            "proposition 2.61"
-            [ testProperty "c" $ testC gen (-*)
+            "proposition 2.64"
+            [ testProperty "c" $ prop64c gen (-*),
+              testProperty "d" $ prop64d gen (-*)
             ]
         ]
