48cdf85729e4d93f8566afa9b6f6112dddce8657
Edwin Tellman <etellman@pobox.com>
2024-06-28 07:45:50 -0700

    Use cost instead of int

--- src/Ch4/Sec1/Example9.hs
+++ src/Ch4/Sec1/Example9.hs
@@ -9,45 +9,46 @@ where
 
 -- import Data.Monoid (Sum (..))
 import Lib.VCategory (VCategory (..))
+import Monoid.Cost
 
 data X = A | B | C | D deriving (Show, Eq)
 
-instance VCategory X Int where
-  hom A B = 6
-  hom A C = 3
-  hom A D = 11
-  hom B A = 2
-  hom B C = 5
-  hom B D = 5
-  hom C A = 5
-  hom C B = 3
-  hom C D = 8
-  hom D A = 9
-  hom D B = 7
-  hom D C = 4
-  hom _ _ = 0
+instance VCategory X Cost where
+  hom A B = Cost 6
+  hom A C = Cost 3
+  hom A D = Cost 11
+  hom B A = Cost 2
+  hom B C = Cost 5
+  hom B D = Cost 5
+  hom C A = Cost 5
+  hom C B = Cost 3
+  hom C D = Cost 8
+  hom D A = Cost 9
+  hom D B = Cost 7
+  hom D C = Cost 4
+  hom _ _ = Cost 0
 
 data Y = X | Y | Z deriving (Show, Eq)
 
-instance VCategory Y Int where
-  hom X Y = 4
-  hom X Z = 3
-  hom Y X = 3
-  hom Y Z = 6
-  hom Z X = 7
-  hom Z Y = 4
-  hom _ _ = 0
+instance VCategory Y Cost where
+  hom X Y = Cost 4
+  hom X Z = Cost 3
+  hom Y X = Cost 3
+  hom Y Z = Cost 6
+  hom Z X = Cost 7
+  hom Z Y = Cost 4
+  hom _ _ = Cost 0
 
 data XY = XY !X !Y deriving (Show, Eq)
 
-instance VCategory XY Int where
-  hom (XY x y) (XY x' y') = hom x' x + hom y y'
+instance VCategory XY Cost where
+  hom (XY x y) (XY x' y') = hom x' x <> hom y y'
 
-bridge :: X -> Y -> Int
-bridge B X = 11
-bridge D Y = 9
-bridge _ _ = undefined
+bridge :: X -> Y -> Cost
+bridge B X = Cost 11
+bridge D Y = Cost 9
+bridge _ _ = Infinity
 
 -- | what is the cost to go from x -> x' -> y' -> y?
-cost :: X -> X -> Y -> Y  -> Int
-cost x x' y' y = hom x x' + bridge x' y' + hom y' y
+cost :: X -> X -> Y -> Y -> Cost
+cost x x' y' y = hom x x' <> bridge x' y' <> hom y' y
--- test/Ch4/Sec1/Exercise10Test.hs
+++ test/Ch4/Sec1/Exercise10Test.hs
@@ -1,6 +1,7 @@
 module Ch4.Sec1.Exercise10Test (tests) where
 
 import Ch4.Sec1.Example9
+import Monoid.Cost
 import Test.Tasty
 import Test.Tasty.HUnit as HU
 
@@ -10,26 +11,26 @@ tests =
     "Ch4.Sec1.Exercise10Test"
     [ testGroup
         "A"
-        [ testCase "x" $ cost A B X X @?= 17,
-          testCase "y" $ cost A D Y Y @?= 20,
-          testCase "z" $ cost A B X Z @?= 20
+        [ testCase "x" $ cost A B X X @?= Cost 17,
+          testCase "y" $ cost A D Y Y @?= Cost 20,
+          testCase "z" $ cost A B X Z @?= Cost 20
         ],
       testGroup
         "B"
-        [ testCase "x" $ cost B B X X @?= 11,
-          testCase "y" $ cost B D Y Y @?= 14,
-          testCase "z" $ cost B B X Z @?= 14
+        [ testCase "x" $ cost B B X X @?= Cost 11,
+          testCase "y" $ cost B D Y Y @?= Cost 14,
+          testCase "z" $ cost B B X Z @?= Cost 14
         ],
       testGroup
         "C"
-        [ testCase "x" $ cost C B X X @?= 14,
-          testCase "y" $ cost C D Y Y @?= 17,
-          testCase "z" $ cost C B X Z @?= 17
+        [ testCase "x" $ cost C B X X @?= Cost 14,
+          testCase "y" $ cost C D Y Y @?= Cost 17,
+          testCase "z" $ cost C B X Z @?= Cost 17
         ],
       testGroup
         "D"
-        [ testCase "x" $ cost D D Y X @?= 12,
-          testCase "y" $ cost D D Y Y @?= 9,
-          testCase "z" $ cost D D Y Z @?= 15
+        [ testCase "x" $ cost D D Y X @?= Cost 12,
+          testCase "y" $ cost D D Y Y @?= Cost 9,
+          testCase "z" $ cost D D Y Z @?= Cost 15
         ]
     ]
