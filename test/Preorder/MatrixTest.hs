e9cbd05c9af98d469ffb7ca83e6c9242d093c6d1
Edwin Tellman <etellman@pobox.com>
2024-07-01 07:27:34 -0700

    Add matrix power demo

--- test/Preorder/MatrixTest.hs
+++ test/Preorder/MatrixTest.hs
@@ -1,12 +1,47 @@
 module Preorder.MatrixTest (tests) where
 
--- import Data.Matrix
+import Data.Matrix
+import Numeric.Natural
 import Test.Tasty
 import Test.Tasty.HUnit
+
+multPower :: Num a => Matrix a -> Natural -> Matrix a
+multPower x 1 = x
+multPower x n = multPower (multStd x x) (n - 1)
 
 tests :: TestTree
 tests =
   testGroup
     "Preorder.MatrixTest"
-    [ testCase "addition" $ 1 @?= (1 :: Int)
+    [ testCase "multiplication" $
+        do
+          let x = matrix 3 3 $ \(i, _) -> i
+              y = matrix 3 3 $ \(_, j) -> j
+
+          multStd x y
+            @?= fromLists
+              [ [3, 6, 9],
+                [6, 12, 18],
+                [9, 18, 27]
+              ],
+      testGroup
+        "power"
+        [ testCase
+            "one"
+            $ do
+              let x = matrix 3 3 $ \(i, _) -> i
+
+              multPower x 1 @?= x,
+          testCase
+            "cube"
+            $ do
+              let x = matrix 3 3 $ \(i, _) -> i
+
+              multPower x 3
+                @?= fromLists
+                  [ [216, 216, 216],
+                    [432, 432, 432],
+                    [648, 648, 648]
+                  ]
+        ]
     ]
