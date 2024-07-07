4e617b27d377cc0b0206e10986c9b7a343ff4f70
Edwin Tellman <etellman@pobox.com>
2024-07-07 06:37:07 -0700

    Alternate implementation of matrix multiply

--- src/Preorder/Quantale.hs
+++ src/Preorder/Quantale.hs
@@ -1,27 +1,30 @@
-module Preorder.Quantale (EdgeCost (..), multPower) where
+module Preorder.Quantale
+  ( distances,
+    multStd3,
+  )
+where
 
 import Data.Matrix
-import Monoid.Cost
-import Numeric.Natural
+import Data.Vector as V
 
-multPower :: Num a => Matrix a -> Natural -> Matrix a
+multPower :: (Num a, Ord a) => Matrix a -> Int -> Matrix a
 multPower x 1 = x
-multPower x n = multPower (multStd x x) (n - 1)
+multPower x n = multPower (multStd3 x x) (n - 1)
 
-data EdgeCost = EdgeCost (Cost Int) deriving (Eq, Ord)
+distances :: (Num a, Ord a) => Matrix a -> Matrix a
+distances x = multPower x (nrows x)
 
-instance Show EdgeCost where
-  show (EdgeCost x) = show x
+combine :: (Num a, Ord a) => Vector a -> Vector a -> a
+combine xs ys =
+  let zipped = V.zipWith (+) xs ys
+   in V.foldr1 min zipped
 
-instance Num EdgeCost where
-  (EdgeCost x) * (EdgeCost y) = EdgeCost (x + y)
-
-  -- adding anything to produces the original value, to preserve normal rules of addition
-  (EdgeCost 0) + x = x
-  x + (EdgeCost 0) = x
-  x + y = min x y
-
-  abs = id
-  signum _ = 1
-  negate (EdgeCost x) = EdgeCost $ negate x
-  fromInteger x = EdgeCost $ fromInteger x
+multStd3 :: (Num a, Ord a) => Matrix a -> Matrix a -> Matrix a
+multStd3 x y =
+  let rows = Prelude.map (flip getRow x) [1 .. nrows x]
+      cols = Prelude.map (flip getCol y) [1 .. ncols y]
+      zipped = do
+        r <- rows
+        c <- cols
+        return $ combine r c
+   in Data.Matrix.fromList (nrows x) (ncols y) zipped
--- test/Preorder/QuantaleTest.hs
+++ test/Preorder/QuantaleTest.hs
@@ -1,110 +1,48 @@
 module Preorder.QuantaleTest (tests) where
 
 import Data.Matrix
-import Gen.Cost
-import Hedgehog as H
-import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
 import Monoid.Cost
 import Preorder.Quantale
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.Hedgehog
-
-prop_multiplyFinite :: Property
-prop_multiplyFinite = property $ do
-  -- set up
-  v1 <- forAll $ Gen.int $ Range.linear 1 1000
-  v2 <- forAll $ Gen.int $ Range.linear 1 1000
-
-  -- exercise
-  let p = (EdgeCost $ Cost v1) * (EdgeCost $ Cost v2)
-
-  -- verify
-  p === EdgeCost (Cost (v1 + v2))
-
-prop_multiplyInfinite :: Property
-prop_multiplyInfinite = property $ do
-  -- set up
-  c <- forAll $ EdgeCost <$> genCost (Range.linear 1 1000)
-
-  -- exercise
-  let p = (EdgeCost Infinity) * c
-
-  -- verify
-  p === EdgeCost Infinity
-
-prop_plusInfinity :: Property
-prop_plusInfinity = property $ do
-  -- set up
-  c <- forAll $ EdgeCost <$> genCost (Range.linear 1 1000)
-
-  -- exercise and verify
-  c + (EdgeCost Infinity) === c
-
-prop_plusPositive :: Property
-prop_plusPositive = property $ do
-  -- mi
-  v1 <- forAll $ Gen.int $ Range.linear 1 100
-  v2 <- forAll $ Gen.int $ Range.linear (v1 + 1) 200
-
-  -- exercise and verify
-  (EdgeCost $ Cost v1) + (EdgeCost $ Cost v2) === EdgeCost (Cost v1)
-
-prop_plusZero :: Property
-prop_plusZero = property $ do
-  -- mi
-  c <- forAll $ EdgeCost <$> genCost (Range.linear 0 1000)
-  let z = EdgeCost $ Cost 0
-
-  -- exercise and verify
-  z + c === c
-  c + z === c
 
 tests :: TestTree
 tests =
   testGroup
     "Preorder.QuantaleTest"
-    [ testCase "multiply" $
+    [ testCase "Example 2.76" $
         do
           let x =
                 fromLists $
-                  (fmap . fmap)
-                    EdgeCost
-                    [ [Infinity, Infinity],
-                      [Infinity, 1],
-                      [1, 1]
-                    ]
+                  [ [Infinity, Infinity],
+                    [Infinity, 1 :: Cost Int],
+                    [1, 1]
+                  ]
               y =
                 fromLists $
-                  (fmap . fmap)
-                    EdgeCost
-                    [ [1, 1, Infinity],
-                      [1, Infinity, 1]
-                    ]
+                  [ [1, 1, Infinity],
+                    [1, Infinity, 1]
+                  ]
               z =
                 fromLists $
-                  (fmap . fmap)
-                    EdgeCost
-                    [ [Infinity, Infinity, Infinity],
-                      [2, Infinity, 2],
-                      [2, 2, 2]
-                    ]
-          multStd x y @?= z,
-      testGroup
-        "EdgeCost"
-        [ testGroup
-            "plus"
-            [ testProperty "infinite" prop_plusInfinity,
-              testProperty "finite" prop_plusPositive,
-              testProperty "zero" prop_plusZero,
-              testCase "0 + infinity" $ EdgeCost (Cost 0) + EdgeCost Infinity @?= EdgeCost Infinity,
-              testCase "infinity + 0" $ EdgeCost Infinity + EdgeCost (Cost 0) @?= EdgeCost Infinity
-            ],
-          testGroup
-            "multiply"
-            [ testProperty "finite" prop_multiplyFinite,
-              testProperty "infinite" prop_multiplyInfinite
-            ]
-        ]
+                  [ [Infinity, Infinity, Infinity],
+                    [2, Infinity, 2],
+                    [2, 2, 2]
+                  ]
+          multStd3 x y @?= z,
+      testCase "Equation 2.18" $
+        do
+          let x =
+                fromLists $
+                  [ [0, 4, (3 :: Cost Int)],
+                    [3, 0, Infinity],
+                    [Infinity, 4, 0]
+                  ]
+              z =
+                fromLists $
+                  [ [0, 4, 3],
+                    [3, 0, 6],
+                    [7, 4, 0]
+                  ]
+          distances x @?= z
     ]
