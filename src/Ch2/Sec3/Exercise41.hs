a9a93dfd4ffea6da3046f1bef21bae20e69638d8
Edwin Tellman <etellman@pobox.com>
2024-07-12 06:03:49 -0700

    Exercise 2.41 use matrix for YNM

--- src/Ch2/Sec3/Exercise41.hs
+++ src/Ch2/Sec3/Exercise41.hs
@@ -1,10 +1,9 @@
 module Ch2.Sec3.Exercise41
   ( ProbabilityWeight (..),
-    ynmArrowsFrom,
+    YesNoMaybe (..),
   )
 where
 
-import Ch2.Sec2.YesNoMaybe
 import Ch2.Sec3.Figure18
 import Graph.Arrow
 
@@ -17,24 +16,7 @@ instance Semigroup ProbabilityWeight where
 instance Ord ProbabilityWeight where
   ProbabilityWeight x <= ProbabilityWeight y = y <= x
 
-categorizeYnm :: Int -> YesNoMaybe
-categorizeYnm d
-  | d >= 5 = Yes
-  | d >= 3 = Maybe
-  | otherwise = No
+data YesNoMaybe = Yes | Maybe | No deriving (Eq, Ord, Show)
 
-data YnmArrow = YnmArrow
-  { ynmFrom :: !Vertex,
-    ynmTo :: !Vertex,
-    ynmWeight :: !YnmMin
-  }
-
-instance Arrow YnmArrow Vertex YnmMin where
-  source = ynmFrom
-  target = ynmTo
-  weight = ynmWeight
-
-ynmArrowsFrom :: Vertex -> [YnmArrow]
-ynmArrowsFrom v =
-  let arrows = arrowsFrom v
-   in fmap (\(Fig18Arrow v1 v2 w) -> YnmArrow v1 v2 (YnmMin . categorizeYnm $ w)) arrows
+instance Semigroup YesNoMaybe where
+  (<>) = max
--- test/Ch2/Sec3/Exercise41Test.hs
+++ test/Ch2/Sec3/Exercise41Test.hs
@@ -1,53 +1,50 @@
 module Ch2.Sec3.Exercise41Test (tests) where
 
-import Ch2.Sec2.YesNoMaybe
 import Ch2.Sec3.Exercise41
-import Ch2.Sec3.Figure18
 import Data.Matrix
-import Graph.Path
 import Preorder.Quantale
 import Test.Tasty
 import Test.Tasty.HUnit
 
-testYnm :: Vertex -> [YesNoMaybe] -> [TestTree]
-testYnm v ws =
-  let test (v2, w) = testCase (show v2) $ maxPath ynmArrowsFrom v v2 @?= Just w
-   in fmap test (zip vertices $ fmap YnmMin ws)
+probabilities :: Matrix ProbabilityWeight
+probabilities =
+  fromLists $
+    (fmap . fmap)
+      ProbabilityWeight
+      [ [1, 0, 1 / 2, 0],
+        [1 / 3, 1, 0, 5 / 6],
+        [0, 1 / 2, 1, 0],
+        [0, 0, 1, 1]
+      ]
+
+toYnm :: ProbabilityWeight -> YesNoMaybe
+toYnm (ProbabilityWeight 0) = No
+toYnm (ProbabilityWeight 1) = Yes
+toYnm (ProbabilityWeight _) = Maybe
 
 tests :: TestTree
 tests =
   testGroup
     "Ch2.Sec3.Exercise41Test"
-    [ testGroup
-        "yes/no/maybe"
-        [ testGroup "A ->" $ testYnm A [Yes, Maybe, Maybe, Maybe],
-          testGroup "B ->" $ testYnm B [No, Yes, Yes, Yes],
-          testGroup "C ->" $ testYnm C [No, Maybe, Yes, Maybe],
-          testGroup "D ->" $ testYnm D [No, Maybe, Yes, Yes]
-        ],
-      testGroup
-        "Ch2.Sec3.Exercise39Test"
-        [
-        -- interpret the original weights sixths
-        testCase "probabilities" $ do
-            let weights =
-                  fromLists $
-                    (fmap . fmap)
-                      ProbabilityWeight
-                      [ [1, 0, 1 / 2, 0],
-                        [1 / 3, 1, 0, 5 / 6],
-                        [0, 1 / 2, 1, 0],
-                        [0, 0, 1, 1]
-                      ]
-                expected =
-                  fromLists $
-                    (fmap . fmap)
-                      ProbabilityWeight
-                      [ [1, 1 / 4, 1 / 2, 5 / 24],
-                        [1 / 3, 1, 5 / 6, 5 / 6],
-                        [1 / 6, 1 / 2, 1, 5 / 12],
-                        [1 / 6, 1 / 2, 1, 1]
-                      ]
-            distances weights @?= expected
-        ]
+    [ testCase "Probability" $ do
+        let expected =
+              fromLists $
+                (fmap . fmap)
+                  ProbabilityWeight
+                  [ [1, 1 / 4, 1 / 2, 5 / 24],
+                    [1 / 3, 1, 5 / 6, 5 / 6],
+                    [1 / 6, 1 / 2, 1, 5 / 12],
+                    [1 / 6, 1 / 2, 1, 1]
+                  ]
+        distances probabilities @?= expected,
+      testCase "Yes/No/Maybe" $ do
+        let weights = fmap toYnm probabilities
+            expected =
+              fromLists $
+                [ [Yes, Maybe, Maybe, Maybe],
+                  [Maybe, Yes, Maybe, Maybe],
+                  [Maybe, Maybe, Yes, Maybe],
+                  [Maybe, Maybe, Yes, Yes]
+                ]
+        distances weights @?= expected
     ]
