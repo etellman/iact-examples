b93d6e12ef8160b80fbe68a31395b91eebb569d7
Edwin Tellman <etellman@pobox.com>
2024-07-14 06:37:50 -0700

    Simplify example 2.31

--- src/Ch2/Sec3/Example31.hs
+++ src/Ch2/Sec3/Example31.hs
@@ -1,16 +1,11 @@
-module Ch2.Sec3.Example31
-  ( ex31,
-    lte31,
-    Ex31 (..),
-  )
-where
+module Ch2.Sec3.Example31 (Ex31 (..)) where
 
 import Data.Char (ord)
 import Data.Matrix
 import Preorder.Quantale
 
-ex31 :: Matrix BoolWeight
-ex31 =
+ex31Edges :: Matrix BoolWeight
+ex31Edges =
   fromLists $
     (fmap . fmap)
       BoolWeight
@@ -21,16 +16,15 @@ ex31 =
         [False, False, False, False, True]
       ]
 
-lte31 :: Char -> Char -> Bool
-lte31 x y =
-  let f = distanceFunc ex31 (\v -> ord v - ord 'p' + 1)
-      BoolWeight w = f x y
-   in w
-
 newtype Ex31 = Ex31 Char deriving (Eq)
 
 instance Show Ex31 where
   show (Ex31 v) = show v
 
+ex31Distance :: Char -> Char -> BoolWeight
+ex31Distance = distanceFunc ex31Edges (\v -> ord v - ord 'p' + 1)
+
 instance Ord Ex31 where
-  Ex31 x <= Ex31 y = x `lte31` y
+  Ex31 x <= Ex31 y =
+    let (BoolWeight w) = ex31Distance x y
+     in w
--- test/Ch2/Sec3/Example31Test.hs
+++ test/Ch2/Sec3/Example31Test.hs
@@ -1,15 +1,15 @@
 module Ch2.Sec3.Example31Test (tests) where
 
 import Ch2.Sec3.Example31
-import Data.Matrix
 import Data.Monoid (All (All))
 import Hedgehog as H
 import qualified Hedgehog.Gen as Gen
 import Monoid.BooleanMonoids (PartialOrdAll (PartialOrdAll))
-import Preorder.Quantale
 import qualified Properties.VCategoryProperties as VC
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Hedgehog
+import TestLib.Assertions ((==>))
 
 genEx31 :: Gen Ex31
 genEx31 = Gen.element $ fmap Ex31 ['p', 'q', 'r', 's', 't']
@@ -17,24 +17,26 @@ genEx31 = Gen.element $ fmap Ex31 ['p', 'q', 'r', 's', 't']
 ex31hom :: Ex31 -> Ex31 -> PartialOrdAll
 ex31hom x y = PartialOrdAll . All $ x <= y
 
+prop_reflexive :: Property
+prop_reflexive = property $ do
+  -- set up
+  x <- forAll genEx31
+  y <- forAll genEx31
+
+  cover 5 "same" $ x == y
+  cover 20 "different" $ x /= y
+
+  -- exercise and verify
+  x <= y && y <= x ==> x == y
+
 tests :: TestTree
 tests =
   testGroup
     "Ch2.Sec3.Example31Test"
-    [ testCase "distance matrix" $ do
-        let expected =
-              fromLists $
-                (fmap . fmap)
-                  BoolWeight
-                  [ [True, True, True, True, True],
-                    [False, True, False, True, True],
-                    [False, False, True, True, True],
-                    [False, False, False, True, True],
-                    [False, False, False, False, True]
-                  ]
-        distances ex31 @?= expected,
-      testCase "p <= q" $ assertBool "<=" $ 'p' `lte31` 'q',
-      testCase "p <= t" $ assertBool "<=" $ 'p' `lte31` 't',
-      testCase "q > p" $ assertBool "<=" $ not ('q' `lte31` 'p'),
-      VC.vCategoryTests "V-Category" genEx31 ex31hom
+    [ VC.vCategoryTests "V-Category" genEx31 ex31hom,
+      testCase "p <= q" $ assertBool "<=" $ Ex31 'p' <= Ex31 'q',
+      testCase "p <= t" $ assertBool "<=" $ Ex31 'p' <= Ex31 't',
+      testCase "q <= t" $ assertBool "<=" $ Ex31 'q' <= Ex31 't',
+      testCase "q > p" $ assertBool "<=" $ Ex31 'q' > Ex31 'p',
+      testProperty "reflexive" prop_reflexive
     ]
