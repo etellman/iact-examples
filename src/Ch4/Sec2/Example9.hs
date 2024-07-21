355dc5b60f904d0e5ec614710669d50c14a6f5f2
Edwin Tellman <etellman@pobox.com>
2024-07-21 06:19:27 -0700

    Example 9 with matrix multiplication

--- src/Ch4/Sec2/Example9.hs
+++ src/Ch4/Sec2/Example9.hs
@@ -19,18 +19,19 @@ xOrd B = 2
 xOrd C = 3
 xOrd D = 4
 
+xEdges :: Matrix IntCost
+xEdges =
+  fromLists $
+    (fmap . fmap)
+      IntCost
+      [ [Cost 0, Infinity, Cost 3, Infinity],
+        [Cost 2, Cost 0, Infinity, Cost 5],
+        [Infinity, Cost 3, Cost 0, Infinity],
+        [Infinity, Infinity, Cost 4, Cost 0]
+      ]
+
 xDistance :: X -> X -> IntCost
-xDistance =
-  let costs =
-        fromLists $
-          (fmap . fmap)
-            IntCost
-            [ [Cost 0, Infinity, Cost 3, Infinity],
-              [Cost 2, Cost 0, Infinity, Cost 5],
-              [Infinity, Cost 3, Cost 0, Infinity],
-              [Infinity, Infinity, Cost 4, Cost 0]
-            ]
-   in distanceFunc costs xOrd xOrd
+xDistance = distanceFunc xEdges xOrd xOrd
 
 instance VCategory X IntCost where
   hom = xDistance
@@ -42,17 +43,18 @@ yOrd X = 1
 yOrd Y = 2
 yOrd Z = 3
 
+yEdges :: Matrix IntCost
+yEdges =
+  fromLists $
+    (fmap . fmap)
+      IntCost
+      [ [Cost 0, Cost 4, Cost 3],
+        [Cost 3, Cost 0, Infinity],
+        [Infinity, Cost 3, Cost 0]
+      ]
+
 yDistance :: Y -> Y -> IntCost
-yDistance =
-  let costs =
-        fromLists $
-          (fmap . fmap)
-            IntCost
-            [ [Cost 0, Cost 4, Cost 3],
-              [Cost 3, Cost 0, Infinity],
-              [Infinity, Cost 3, Cost 0]
-            ]
-   in distanceFunc costs yOrd yOrd
+yDistance = distanceFunc yEdges yOrd yOrd
 
 instance VCategory Y IntCost where
   hom = yDistance
@@ -62,19 +64,18 @@ data XY = XY !X !Y deriving (Show, Eq)
 instance VCategory XY IntCost where
   hom (XY x y) (XY x' y') = hom x' x <> hom y y'
 
-bridge :: X -> Y -> IntCost
-bridge B X = IntCost $ Cost 11
-bridge D Y = IntCost $ Cost 9
-bridge _ _ = IntCost $ Infinity
-
--- | what is the cost to go from x to y via x' and y'
-cost' :: X -> Y -> X -> Y -> Cost Int
-cost' x y x' y' =
-  let (IntCost c) = hom x x' <> bridge x' y' <> hom y' y
-   in c
-
--- | what is the minimum cost to go from to y?
+-- | what is the minimum cost to go from x to y?
 cost :: X -> Y -> Cost Int
 cost x y =
-  let c = cost' x y
-   in min (c B X) (c D Y)
+  let bridges =
+        fromLists $
+          (fmap . fmap)
+            IntCost
+            [ [Infinity, Infinity, Infinity],
+              [Cost 11, Infinity, Infinity],
+              [Infinity, Infinity, Infinity],
+              [Infinity, Cost 9, Infinity]
+            ]
+      xyDstances = (distances xEdges) `quantMult` bridges `quantMult` (distances yEdges)
+      (IntCost d) = getElem (xOrd x) (yOrd y) xyDstances
+   in d
