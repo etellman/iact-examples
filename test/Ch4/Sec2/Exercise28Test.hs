2b3b748772d74f2ea5e845b228382117796154ee
Edwin Tellman <etellman@pobox.com>
2024-07-28 07:27:05 -0700

    Add exercise 4.28

---
+++ test/Ch4/Sec2/Exercise28Test.hs
@@ -0,0 +1,60 @@
+module Ch4.Sec2.Exercise28Test (tests) where
+
+import Hedgehog as H
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+f :: Num n => (n, n, n) -> n
+f (a, b, c) = a + b + c
+
+companion :: (Ord n, Num n) => (n, n, n) -> n -> Bool
+companion p q = f p <= q
+
+conjoint :: (Ord n, Num n) => n -> (n, n, n) -> Bool
+conjoint q p = q <= f p
+
+prop_companion :: Property
+prop_companion =
+  property $ do
+    -- set up
+    a <- forAll $ Gen.int (Range.constant 1 300)
+    b <- forAll $ Gen.int (Range.constant 1 300)
+    c <- forAll $ Gen.int (Range.constant 1 300)
+    d <- forAll $ Gen.int (Range.constant 1 900)
+
+    cover 30 "<=" $ a + b + c <= d
+    cover 30 ">" $ a + b + c > d
+
+    -- exercise
+    let v = companion (a, b, c) d
+
+    -- verify
+    v === (a + b + c <= d)
+
+prop_conjoint :: Property
+prop_conjoint =
+  property $ do
+    -- set up
+    a <- forAll $ Gen.int (Range.constant 1 900)
+    b <- forAll $ Gen.int (Range.constant 1 300)
+    c <- forAll $ Gen.int (Range.constant 1 300)
+    d <- forAll $ Gen.int (Range.constant 1 300)
+
+    cover 30 "<=" $ a <= b + c + d
+    cover 30 ">" $ a > b + c + d
+
+    -- exercise
+    let v = conjoint a (b, c, d)
+
+    -- verify
+    v === (a <= b + c + d)
+
+tests :: TestTree
+tests =
+  testGroup
+    "Ch4.Sec2.Exercise28Test"
+    [ testProperty "companion" prop_companion,
+      testProperty "conjoint" prop_conjoint
+    ]
--- test/Ch4/Sec2/SectionTest.hs
+++ test/Ch4/Sec2/SectionTest.hs
@@ -7,6 +7,7 @@ import Ch4.Sec2.Example9Test
 import Ch4.Sec2.Exercise10Test
 import Ch4.Sec2.Exercise13Test
 import Ch4.Sec2.Exercise15.Exercise15Test
+import Ch4.Sec2.Exercise28Test
 import Ch4.Sec2.Exercise2Test
 import Ch4.Sec2.Exercise5Test
 import Ch4.Sec2.Exercise8Test
@@ -25,5 +26,6 @@ sectionTests =
       Ch4.Sec2.Example9Test.tests,
       Ch4.Sec2.Exercise10Test.tests,
       Ch4.Sec2.Exercise13Test.tests,
-      Ch4.Sec2.Exercise15.Exercise15Test.exerciseTests
+      Ch4.Sec2.Exercise15.Exercise15Test.exerciseTests,
+      Ch4.Sec2.Exercise28Test.tests
     ]
